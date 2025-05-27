ce90f76dee1267f614ac13e1d3d76bbe16bb0903
Smit105 <smitparekh06@gmail.com>
2025-05-27 12:16:28 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Welcome to Atlas RevX + RevenueMaster AI</h1>
+      <p>Your Revenue. Optimized. 📈</p>
+    </div>
+  );
+}
+
+export default App;
